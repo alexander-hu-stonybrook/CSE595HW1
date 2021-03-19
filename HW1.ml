9b25b6f1d15df7b52fd022b1b9097bc73fa460d9
enigmamemory <enigmamemory@gmail.com>
2021-03-19 19:58:45 -0400

    Finished Part 3 Q1

--- HW1.ml
+++ HW1.ml
@@ -207,3 +207,42 @@ let rec iter_all_terms_in_set plist retlist =
 
 let powerset plist =
     iter_all_terms_in_set plist [[]]
+
+(* Part 3 *)
+
+(* Question 1 *)
+
+type bool_expr =
+    | Lit of string
+    | Not of bool_expr
+    | And of bool_expr * bool_expr
+    | Or of bool_expr * bool_expr
+;;
+
+let possible_inputs = [(true, true); (true, false); (false, true); (false, false)]
+;;
+
+(* Returns a true/false for the entire expression *)
+let rec eval_expr a b booltup inexp =
+    match inexp with
+        |Lit x1 when x1 = a -> fst booltup
+        |Lit x2 when x2 = b -> snd booltup
+        |Lit _ -> false (*Shouldn't ever come up*)
+        |Not subexp -> not (eval_expr a b booltup subexp)
+        |And (sub1, sub2) -> (eval_expr a b booltup sub1) && (eval_expr a b booltup sub2)
+        |Or (sub1, sub2) -> (eval_expr a b booltup sub1) || (eval_expr a b booltup sub2)
+;;
+
+let rec check_all_inputs a b inexp blist retlist =
+    if blist = [] then
+        retlist
+    else
+        check_all_inputs a b inexp (List.tl blist) (List.append retlist
+        [(fst (List.hd blist), snd (List.hd blist), eval_expr a b (List.hd blist) inexp)])
+    ;;
+
+let truth_table a b expr =
+    check_all_inputs a b expr possible_inputs []
+;;
+
+(* Question 2 *)
